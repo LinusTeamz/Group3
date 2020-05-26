843ee5fc7feaebf94b41fa6107ddbff47bb15458
Sarmad Rashid <rashid-89@hotmail.com>
2020-05-26 19:43:15 +0200

    La in adminfunktioner + help controller

--- WebProject/Controllers/AdminController.cs
+++ WebProject/Controllers/AdminController.cs
@@ -11,18 +11,349 @@ namespace WebProject.Controllers
 {
     public class AdminController : Controller
     {
+        ObjectHandlerJSON obj = new ObjectHandlerJSON();
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
 
-        public async System.Threading.Tasks.Task<ActionResult> test()
+        // GET: AdminFacility
+        public async System.Threading.Tasks.Task<ActionResult> FacilityIndex()
         {
-            ObjectHandlerJSON json = new ObjectHandlerJSON();
-            var test = await json.GetFacilityList();
-            
+            try
+            {
+                if (Session["user"] == null || Session["user"].ToString() != "admin")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                List<Facility> model = new List<Facility>();
+                model = await obj.GetFacilityList();
+
+                return View(model);
+            }
+            catch (Exception e)
+            {
+                TempData["tempErrorMessage"] = e.Message.ToString();
+                return RedirectToAction("Error", "Help");
+            }
+
+        }
+
+        // GET: AdminFacility/Create
+        public ActionResult FacilityCreate()
+        {
+            try
+            {
+                if (Session["user"] == null || Session["user"].ToString() != "admin")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                return View();
+            }
+            catch (Exception e)
+            {
+                TempData["tempErrorMessage"] = e.Message.ToString();
+                return RedirectToAction("Error", "Help");
+            }
+        }
+
+        // POST: AdminFacility/Create
+        [HttpPost]
+        public ActionResult FacilityCreate(Facility facility)
+        {
+            try
+            {
+                obj.AddFacility(facility);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminFacility/Edit/5
+        public ActionResult FacilityEdit(int id)
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
+        }
+
+        // POST: AdminFacility/Edit/5
+        [HttpPost]
+        public ActionResult FacilityEdit(int id, Facility facility)
+        {
+            try
+            {
+                obj.UpdateFacility(facility);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminFacility/Delete/5
+        public async System.Threading.Tasks.Task<ActionResult> FacilityDelete(int id)
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            Facility model = new Facility();
+
+            model = await obj.GetFacilityByID(id);
+
+            return View(model);
+        }
+
+        // POST: AdminFacility/Delete/5
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> FacilityDelete(int id, Facility facility)
+        {
+            try
+            {
+                await obj.DeleteFacility(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: AdminOrganizer
+        public async System.Threading.Tasks.Task<ActionResult> OrganizerIndex()
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ObjectHandlerJSON json = new ObjectHandlerJSON();
+            List<Organizer> model = new List<Organizer>();
+            model = await json.GetOrganizerList();
+
+            return View(model);
+        }
+
+        // GET: AdminOrganizer/Details/5
+        public ActionResult OrganizerDetails(int id)
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        // GET: AdminOrganizer/Create
+        public ActionResult OrganizerCreate()
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        // POST: AdminOrganizer/Create
+        [HttpPost]
+        public ActionResult OrganizerCreate(Organizer organizer)
+        {
+            try
+            {
+                obj.AddOrganizer(organizer);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminOrganizer/Edit/5
+        public ActionResult OrganizerEdit(int id)
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        // POST: AdminOrganizer/Edit/5
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> OrganizerEdit(int id, Organizer organizer)
+        {
+            try
+            {
+                await obj.UpdateOrganizer(organizer);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminOrganizer/Delete/5
+        public async System.Threading.Tasks.Task<ActionResult> OrganizerDelete(int id)
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            Organizer model = new Organizer();
+
+            model = await obj.GetOrganizerByID(id);
+
+            return View(model);
+        }
+
+        // POST: AdminOrganizer/Delete/5
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> OrganizerDelete(int id, Place place)
+        {
+            try
+            {
+                await obj.DeleteOrganizer(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: AdminPlace
+        public async System.Threading.Tasks.Task<ActionResult> PlaceIndex()
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ObjectHandlerJSON json = new ObjectHandlerJSON();
+            List<Place> model = new List<Place>();
+            model = await json.GetPlaceList();
+
+            return View(model);
+        }
+
+        // GET: AdminPlace/Details/5
+        public ActionResult PlaceDetails(int id)
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        // GET: AdminPlace/Create
+        public ActionResult PlaceCreate()
+        {
+
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        // POST: AdminPlace/Create
+        [HttpPost]
+        public ActionResult PlaceCreate(Place place)
+        {
+            try
+            {
+                obj.AddPlace(place);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminPlace/Edit/5
+        public ActionResult PlaceEdit(int id)
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        // POST: AdminPlace/Edit/5
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> PlaceEdit(int id, Place place)
+        {
+            try
+            {
+                await obj.UpdatePlace(place);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminPlace/Delete/5
+        public async System.Threading.Tasks.Task<ActionResult> PlaceDelete(int id)
+        {
+            if (Session["user"] == null || Session["user"].ToString() != "admin")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            Place model = new Place();
+
+            model = await obj.GetPlaceByID(id);
+
+            return View(model);
+        }
+
+        // POST: AdminPlace/Delete/5
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> PlaceDelete(int id, Place place)
+        {
+            try
+            {
+                await obj.DeletePlace(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ WebProject/Controllers/HelpController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProject.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult Error()
+        {
+
+            // Skriv enbart ut ifall det finns data
+            if (TempData.ContainsKey("tempErrorMessage"))
+            {
+                // Skriver ut felmeddelandet
+                ViewBag.ErrorMessage = TempData["tempErrorMessage"].ToString();
+
+                // Ta bort temp data efter användning
+                TempData.Remove("tempErrorMessage");
+            }
+
+            return View();
+        }
+    }
+}
