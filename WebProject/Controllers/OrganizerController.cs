21194a78d6b246c60cb01e47c7efcd84eb909add
linusbjo <linusbjo1012@gmail.com>
2020-05-28 13:41:40 +0200

    Fixat felhantering i Organizer

--- WebProject/Controllers/OrganizerController.cs
+++ WebProject/Controllers/OrganizerController.cs
@@ -11,46 +11,78 @@ namespace WebProject.Controllers
     public class OrganizerController : Controller
     {
         ObjectHandlerJSON obj = new ObjectHandlerJSON();
-        // GET: Organizer
+
         public ActionResult Index()
-        {
-            return View();
+        {          
+            try
+            {
+                return View();
+            }
+            catch (Exception e)
+            {
+                TempData["tempErrorMessage"] = e.Message.ToString();
+                return RedirectToAction("Error", "Help");
+            }
         }
         public async System.Threading.Tasks.Task<ActionResult> CreateEvent()
-        {
-            List<EventCategory> categoriesList = new List<EventCategory>();
-            categoriesList = await obj.GetCategoryList();
-            List<SelectListItem> categoryDropDown = new List<SelectListItem>();
-
-            foreach (var item in categoriesList)
+        {          
+            try
             {
-                SelectListItem temp = new SelectListItem();
-                temp.Text = item.Category_Name;
-                temp.Value = item.Category_Id.ToString();
-                categoryDropDown.Add(temp);
+                List<SelectListItem> categoryDropDown = new List<SelectListItem>();
+                List<EventCategory> categoriesList = new List<EventCategory>();
+
+                categoriesList = await obj.GetCategoryList();
+              
+                foreach (var item in categoriesList)
+                {
+                    SelectListItem temp = new SelectListItem();
+                    temp.Text = item.Category_Name;
+                    temp.Value = item.Category_Id.ToString();
+                    categoryDropDown.Add(temp);
+                }
+
+                ViewBag.Category_Id = categoryDropDown;
+
+                return View();
             }
-
-            ViewBag.Category_Id = categoryDropDown;
-
-
-            return View();
+            catch (Exception e)
+            {
+                TempData["tempErrorMessage"] = e.Message.ToString();
+                return RedirectToAction("Error", "Help");
+            }
         }
         // POST: CreateE/Create
         [HttpPost]
 
         public async System.Threading.Tasks.Task<ActionResult> CreateEvent(Event newEvent, int Category_Id)
-        {
-            newEvent.Event_Category = new EventCategory();
-            newEvent.Event_Category.Category_Id = Category_Id;
-            newEvent.Event_Create_Datetime = DateTime.Now;
-            await obj.AddEvent(newEvent);
-            
-            return RedirectToAction("Index", "Organizer");
+        {      
+            try
+            {
+                newEvent.Event_Category = new EventCategory();
+                newEvent.Event_Category.Category_Id = Category_Id;
+                newEvent.Event_Create_Datetime = DateTime.Now;
+                await obj.AddEvent(newEvent);
+                
+                return RedirectToAction("Index", "Organizer");
+            }
+            catch (Exception e)
+            {
+                TempData["tempErrorMessage"] = e.Message.ToString();
+                return RedirectToAction("Error", "Help");
+            }
         }
 
         public ActionResult MyEvent()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception e)
+            {
+                TempData["tempErrorMessage"] = e.Message.ToString();
+                return RedirectToAction("Error", "Help");
+            }
         }
     }
 }
